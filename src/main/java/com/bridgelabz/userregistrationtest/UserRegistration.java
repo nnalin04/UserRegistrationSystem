6e99a7a4c8ba6318d4362b13fbde8511b52c90e3
nnalin04 <nnalin04@gmail.com>
2020-08-12 01:07:42 +0530

    added: function to check the valid email.

--- src/main/java/com/bridgelabz/userregistrationtest/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistrationtest/UserRegistration.java
@@ -4,10 +4,16 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}([ ][A-Z]{1}[a-z]{2,})*";
+    private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}([ ][A-Z]{1}[a-z]{2,})*";
+    private static final String EMAIL_PATTERN = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[0-9a-zA-Z]{2,4}([.][a-zA-Z]{2})*$";
 
     public boolean registerName(String fname) {
-        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matcher(fname).matches();
+    }
+
+    public boolean registerEmail(String email){
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(email).matches();
     }
 }
