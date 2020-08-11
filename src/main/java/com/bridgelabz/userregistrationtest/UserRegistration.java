fc740b78e77d56893f3a3fd594663a30e96bb3dc
nnalin04 <nnalin04@gmail.com>
2020-08-12 01:00:02 +0530

    added: function to check the valid lastName.

--- src/main/java/com/bridgelabz/userregistrationtest/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistrationtest/UserRegistration.java
@@ -4,9 +4,9 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2}";
+    private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}([ ][A-Z]{1}[a-z]{2,})*";
 
-    public boolean registerFirstName(String fname) {
+    public boolean registerName(String fname) {
         Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
         return pattern.matcher(fname).matches();
     }
